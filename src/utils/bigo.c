923619c575ed7e14838bb3b2f8f198bfb62100f6
Reid Paape <wrpaape@gmail.com>
2016-05-06 09:52:59 -0500

    doing away with macros for now

--- src/utils/bigo.c
+++ src/utils/bigo.c
@@ -4,25 +4,23 @@ extern inline char *time_complexity_class(const enum TimeComplexityClass tcc);
 extern inline char *time_complexity_order(const enum TimeComplexityClass tcc);
 extern inline char *time_complexity_rating(const enum TimeComplexityClass tcc);
 
-struct BenchmarkPoint {
-	size_t n;
-	clock_t ticks;
-};
 
-#define PUT_POINT(point, FN, N, INPUTS)
-do {
-	point->n = N;
-	point->ticks = clock();
-	FN INPUTS;
-	point->ticks = clock() - point->ticks;
-} while (0)
 
-#define BENCHMARK_SETUP(RES, FN, TUP_CNT, INPUT_TUPS...)
-do {
-	struct BenchmarkPoint points[TUP_CNT];
-	struct BenchmarkPoint *point = &points[0l];
+enum TimeComplexityClass approximate_time_complexity(const size_t n_min,
+						     const size_t n_max,
+						     const size_t n_res,
+						     struct timeval *restrict timeout,
+						     void (*range_generator)(const size_t *,
+									     const size_t,
+									     const size_t,
+									     const size_t),
+						     clock_t (*function_timer)(const size_t))
+{
 
-	PUT_POINTS(point, FN, INPUT_TUPS);
+	clock_t n_ticks[n_res];
+	size_t n_range[n_res];
+	long long double n_ratios
 
-	RES = approximate_time_complexity(&points[0l], TUP_CNT);
-} while (0)
+
+	return tc_clas;
+}
