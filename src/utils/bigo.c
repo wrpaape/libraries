2ae4abceca9a75fbc3ab85ecc8c54bb8fb8583c5
Reid Paape <wrpaape@gmail.com>
2016-05-06 11:32:50 -0500

    figuring out input relevancy cutoffs for testing extreme growth rates

--- src/utils/bigo.c
+++ src/utils/bigo.c
@@ -1,25 +1,45 @@
+#include <utils/utils.h>
 #include <utils/bigo.h>
 
 extern inline char *time_complexity_class(const enum TimeComplexityClass tcc);
 extern inline char *time_complexity_order(const enum TimeComplexityClass tcc);
 extern inline char *time_complexity_rating(const enum TimeComplexityClass tcc);
 
+struct SizeStream {
+	size_t min;
+	size_t max;
+	size_t res;
+	void (*generator)(const size_t *,
+			  const size_t,
+			  const size_t,
+			  const size_t)
+};
 
+long double time_over_1(const clock_t time, const size_t n);
+long double time_over_log_n(const clock_t time, const size_t n);
+long double time_over_n(const clock_t time, const size_t n);
+long double time_over_n_log_n(const clock_t time, const size_t n);
+long double time_over_n_squared(const clock_t time, const size_t n);
+long double time_over_n_cubed(const clock_t time, const size_t n);
+long double time_over_exp_n(const clock_t time, const size_t n);
+long double time_over_n_factorial(const clock_t time, const size_t n);
 
-enum TimeComplexityClass approximate_time_complexity(const size_t n_min,
-						     const size_t n_max,
-						     const size_t n_res,
+long double (*RATIO_MAP[])
+
+enum TimeComplexityClass approximate_time_complexity(struct SizeStream *restrict n_range,
 						     struct timeval *restrict timeout,
-						     void (*range_generator)(const size_t *,
-									     const size_t,
-									     const size_t,
-									     const size_t),
 						     clock_t (*function_timer)(const size_t))
 {
 
-	clock_t n_ticks[n_res];
-	size_t n_range[n_res];
-	long long double n_ratios
+	if (n_range->max < n_range->min)
+		EXIT_ON_FAILURE("maximum input 'n' ( %zu ) exceeds minimum ( %zu )",
+				n_range->max, n_range->min);
+
+
+
+	clock_t ticks[n_range->res];
+	size_t input_n[n_range->res];
+	long double n_ratios[];
 
 
 	return tc_clas;
