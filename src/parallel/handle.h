7f30412a6111dae12ea070a13add12b46d1a87b2
Reid Paape <wrpaape@gmail.com>
2016-05-09 08:25:00 -0500

    almost done with handlers

---
+++ src/parallel/handle.h
@@ -0,0 +1,546 @@
+#ifndef PARALLEL_HANDLE_H_
+#define PARALLEL_HANDLE_H_
+
+#ifdef __cplusplus /* ensure C linkage */
+extern "C" {
+#ifndef restrict /* replace 'restrict' with c++ compatible '__restrict' */
+#define restrict __restrict__
+#endif
+#endif
+
+
+/* FUNCTION-LIKE MACROS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+/* pthread_t
+ * ========================================================================== */
+/* pthread_create */
+#define HANDLE_PTHREAD_CREATE(THREAD, ATTR, ROUTINE, ARG)		\
+do {									\
+	int status = pthread_create(THREAD, ATTR, ROUTINE, ARG);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to create pthread"		\
+				"\e24m]\n\n{\n"				\
+				"\tthread:        '" #THREAD  "'\n"	\
+				"\tattr:          '" #ATTR    "'\n"	\
+				"\tstart_routine: '" #ROUTINE "'\n"	\
+				"\targ:           '" #ARG     "'\n"	\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_CREATE_FAILURE(status));		\
+} while (0)
+#define PT_CREATE_FAILURE(STATUS)					\
+((STATUS == EAGAIN)							\
+? "The system lacked the necessary resources to create another thread"	\
+  ", or the system-imposed limit on the total number of threads in a "	\
+  "process [PTHREAD_THREADS_MAX] would be exceeded."			\
+: ((STATUS == EINVAL)							\
+? "The value specified by attr is invalid."				\
+: "unknown"))
+
+
+/* pthread_join */
+#define HANDLE_PTHREAD_JOIN(THREAD, VALUE_PTR)				\
+do {									\
+	int status = pthread_join(THREAD, VALUE_PTR);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to join pthread"		\
+				"\e24m]\n\n{\n"				\
+				"\tthread:    '" #THREAD    "'\n"	\
+				"\tvalue_ptr: '" #VALUE_PTR "'\n"	\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_JOIN_FAILURE(status));		\
+} while (0)
+#define PT_JOIN_FAILURE(STATUS)					\
+((STATUS == EDEADLK)							\
+? "A deadlock was detected or the value of thread specifies the "	\
+  "calling thread."							\
+: ((STATUS == EINVAL)							\
+? "The implementation has detected that the value specified by thread "	\
+  "does not refer to a joinable thread."				\
+: ((STATUS == ESRCH)							\
+? "No thread could be found corresponding to that specified by the "	\
+  "given thread ID, thread."						\
+: "unknown")))
+
+
+
+/* pthread_mutex_t
+ * ========================================================================== */
+/* pthread_mutex_init */
+#define HANDLE_PTHREAD_MUTEX_INIT(MUTEX, ATTR)				\
+do {									\
+	int status = pthread_mutex_init(MUTEX, ATTR);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to initialize pthread mutex"	\
+				"\e24m]\n\n{\n"				\
+				"\tmutex: '" #MUTEX "'\n"		\
+				"\tattr:  '" #ATTR "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_M_INIT_FAILURE(status));		\
+} while (0)
+#define PT_M_INIT_FAILURE(STATUS)					\
+((STATUS == EAGAIN)							\
+? "The system temporarily lacks the resources to create another mutex."	\
+: ((STATUS == EINVAL)							\
+? "The value specified by 'attr' is invalid."				\
+: ((STATUS == ENOMEM)							\
+? "The process cannot allocate enough memory to create another mutex."	\
+: "unknown")))
+
+
+/* pthread_mutex_lock */
+#define HANDLE_PTHREAD_MUTEX_LOCK(MUTEX)				\
+do {									\
+	int status = pthread_mutex_lock(MUTEX);				\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to lock pthread mutex"		\
+				"\e24m]\n\n{\n"				\
+				"\tmutex: '" #MUTEX "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_M_LOCK_FAILURE(status));		\
+} while (0)
+#define PT_M_LOCK_FAILURE(STATUS)					\
+((STATUS == EDEADLK)							\
+? "A deadlock would occur if the thread is blocked waiting for mutex."	\
+: ((STATUS == EINVAL)							\
+? "The value specified by 'mutex' is invalid."				\
+: "unknown"))
+
+
+/* pthread_mutex_unlock */
+#define HANDLE_PTHREAD_MUTEX_UNLOCK(MUTEX)				\
+do {									\
+	int status = pthread_mutex_unlock(MUTEX);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to unlock pthread mutex"	\
+				"\e24m]\n\n{\n"				\
+				"\tmutex: '" #MUTEX "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_M_UNLOCK_FAILURE(status));		\
+} while (0)
+#define PT_M_UNLOCK_FAILURE(STATUS)					\
+((STATUS == EINVAL)							\
+? "The value specified by 'mutex' is invalid."				\
+: ((STATUS == EPERM)							\
+? "The current thread does not hold a lock on 'mutex'."			\
+: "unknown")
+
+
+
+/* pthread_mutexattr_t
+ * ========================================================================== */
+/* pthread_mutexattr_init */
+#define HANDLE_PTHREAD_MUTEXATTR_INIT(ATTR)				\
+do {									\
+	int status = pthread_mutexattr_init(ATTR);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to initialize pthread mutex "	\
+				"attribute"				\
+				"\e24m]\n\n{\n"				\
+				"\tattr: '" #ATTR "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_INIT_FAILURE(status));		\
+} while (0)
+#define PT_MA_INIT_FAILURE(STATUS)					\
+((STATUS == EAGAIN)							\
+? "Out of memory."							\
+: "unknown")
+
+
+/* pthread_mutexattr_destroy */
+#define HANDLE_PTHREAD_MUTEXATTR_DESTROY(ATTR)				\
+do {									\
+	int status = pthread_mutexattr_destroy(ATTR);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to destroy pthread mutex "	\
+				"attribute"				\
+				"\e24m]\n\n{\n"				\
+				"\tattr: '" #ATTR "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_DESTROY_FAILURE(status));		\
+} while (0)
+#define PT_MA_DESTROY_FAILURE(STATUS)					\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr'."						\
+: "unknown")
+
+
+/* pthread_mutexattr_setprioceiling */
+#define HANDLE_PTHREAD_MUTEXATTR_SETPRIOCEILING(ATTR, PRIOCEILING)	\
+do {									\
+	int status = pthread_mutexattr_setprioceiling(ATTR,		\
+						      PRIOCEILING);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set priority ceiling for "	\
+				"pthread mutex attribute"		\
+				"\e24m]\n\n{\n"				\
+				"\tattr:        '" #ATTR "'\n"		\
+				"\tprioceiling: '" #PRIOCEILING "'\n"	\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_SETPRIOCEILING_FAILURE(status));	\
+} while (0)
+#define PT_MA_SETPRIOCEILING_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr', or invalid value for 'prioceiling'."	\
+: "unknown")
+
+
+/* pthread_mutexattr_getprioceiling */
+#define HANDLE_PTHREAD_MUTEXATTR_GETPRIOCEILING(ATTR, PRIOCEILING)	\
+do {									\
+	int status = pthread_mutexattr_getprioceiling(ATTR,		\
+						      PRIOCEILING);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to get priority ceiling for "	\
+				"pthread mutex attribute"		\
+				"\e24m]\n\n{\n"				\
+				"\tattr:        '" #ATTR "'\n"		\
+				"\tprioceiling: '" #PRIOCEILING "'\n"	\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_GETPRIOCEILING_FAILURE(status));	\
+} while (0)
+#define PT_MA_GETPRIOCEILING_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr'."						\
+: "unknown")
+
+
+/* pthread_mutexattr_setprotocol */
+#define HANDLE_PTHREAD_MUTEXATTR_SETPROTOCOL(ATTR, PROTOCOL)		\
+do {									\
+	int status = pthread_mutexattr_setprotocol(ATTR, PROTOCOL);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set protocol for pthread "	\
+				"mutex attribute"			\
+				"\e24m]\n\n{\n"				\
+				"\tattr:     '" #ATTR "'\n"		\
+				"\tprotocol: '" #PROTOCOL "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_SETPROTOCOL_FAILURE(status));	\
+} while (0)
+#define PT_MA_SETPROTOCOL_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr', or invalid value for 'protocol'."		\
+: "unknown")
+
+
+/* pthread_mutexattr_getprotocol */
+#define HANDLE_PTHREAD_MUTEXATTR_GETPROTOCOL(ATTR, PROTOCOL)		\
+do {									\
+	int status = pthread_mutexattr_getprotocol(ATTR,		\
+						   PROTOCOL);		\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to get protocol for pthread "	\
+				"mutex attribute"			\
+				"\e24m]\n\n{\n"				\
+				"\tattr:     '" #ATTR "'\n"		\
+				"\tprotocol: '" #PROTOCOL "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_GETPROTOCOL_FAILURE(status));	\
+} while (0)
+#define PT_MA_GETPROTOCOL_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr'."						\
+: "unknown")
+
+
+/* pthread_mutexattr_settype */
+#define HANDLE_PTHREAD_MUTEXATTR_SETTYPE(ATTR, TYPE)			\
+do {									\
+	int status = pthread_mutexattr_settype(ATTR, TYPE);		\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set type for pthread mutex "	\
+				"attribute"				\
+				"\e24m]\n\n{\n"				\
+				"\tattr: '" #ATTR "'\n"			\
+				"\ttype: '" #TYPE "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_SETTYPE_FAILURE(status));		\
+} while (0)
+#define PT_MA_SETTYPE_FAILURE(STATUS)					\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr', or invalid value for 'type'."		\
+: "unknown")
+
+
+/* pthread_mutexattr_gettype */
+#define HANDLE_PTHREAD_MUTEXATTR_GETTYPE(ATTR, TYPE)			\
+do {									\
+	int status = pthread_mutexattr_gettype(ATTR, TYPE);		\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to get type for pthread mutex "	\
+				"attribute"				\
+				"\e24m]\n\n{\n"				\
+				"\tattr: '" #ATTR "'\n"			\
+				"\ttype: '" #TYPE "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_GETTYPE_FAILURE(status));		\
+} while (0)
+#define PT_MA_GETTYPE_FAILURE(STATUS)					\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr'."						\
+: "unknown")
+
+
+/* if system supports the 'process-shared/pshared' attribute */
+#ifdef _POSIX_THREAD_PROCESS_SHARED
+
+/* pthread_mutexattr_getpshared */
+#define HANDLE_PTHREAD_MUTEXATTR_GETPSHARED(ATTR, PSHARED)		\
+do {									\
+	int status = pthread_mutexattr_getpshared(ATTR, PSHARED);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to get 'process-shared' "	\
+				"for pthread mutex attribute"		\
+				"\e24m]\n\n{\n"				\
+				"\tattr:    '" #ATTR "'\n"		\
+				"\tpshared: '" #PSHARED "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_GETPSHARED_FAILURE(status));	\
+} while (0)
+#define PT_MA_GETPSHARED_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr'."						\
+: "unknown")
+
+
+/* pthread_mutexattr_setpshared */
+#define HANDLE_PTHREAD_MUTEXATTR_SETPSHARED(ATTR, PSHARED)		\
+do {									\
+	int status = pthread_mutexattr_setpshared(ATTR, PSHARED);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set 'process-shared' "	\
+				"for pthread mutex attribute"		\
+				"\e24m]\n\n{\n"				\
+				"\tattr:    '" #ATTR "'\n"		\
+				"\tpshared: '" #PSHARED "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_MA_SETPSHARED_FAILURE(status));	\
+} while (0)
+#define PT_MA_SETPSHARED_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "The new value specified for the attribute is outside the range of "	\
+  "legal values for that attribute."					\
+: "unknown")
+
+#endif	/* ifdef _POSIX_THREAD_PROCESS_SHARED */
+
+
+/* pthread_cond_t
+ * ========================================================================== */
+/* pthread_cond_init */
+#define HANDLE_PTHREAD_COND_INIT(COND, ATTR)				\
+do {									\
+	int status = pthread_cond_init(COND, ATTR);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set pthread cancel type"	\
+				"\e24m]\n\n{\n"				\
+				"\tcond: '" #COND "'\n"			\
+				"\tattr: '" #ATTR "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_COND_INIT_FAILURE(status));		\
+} while (0)
+#define PT_COND_INIT_FAILURE(STATUS)					\
+((STATUS == EAGAIN)							\
+? "The system temporarily lacks the resources to create another "	\
+  "condition variable."							\
+: ((STATUS == EINVAL)							\
+? "The value specified by 'attr' is invalid."				\
+: ((STATUS == ENOMEM)							\
+? "The process cannot allocate enough memory to create another "	\
+  "condition variable."							\
+: "unknown")))
+
+
+/* pthread_cond_signal */
+#define HANDLE_PTHREAD_COND_SIGNAL(COND)				\
+do {									\
+	int status = pthread_cond_signal(COND);				\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to signal pthread condition"	\
+				"\e24m]\n\n{\n"				\
+				"\tcond: '" #COND "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_COND_SIGNAL_FAILURE(status));	\
+} while (0)
+#define PT_COND_SIGNAL_FAILURE(STATUS)					\
+((STATUS == EINVAL)							\
+? "the value specified by 'cond' is invalid."				\
+: "unknown")
+
+
+/* pthread_cond_timedwait */
+#define HANDLE_PTHREAD_COND_TIMEDWAIT(COND, MUTEX, ABSTIME)		\
+do {									\
+	int status = pthread_cond_timedwait(COND, MUTEX, ABSTIME);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to signal pthread condition"	\
+				"\e24m]\n\n{\n"				\
+				"\tcond:    '" #COND "'\n"		\
+				"\tmutex:   '" #MUTEX "'\n"		\
+				"\tabstime: '" #ABSTIME "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_COND_TIMEDWAIT_FAILURE(status));	\
+} while (0)
+#define PT_COND_TIMEDWAIT_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "The value specified by 'cond', 'mutex' or 'abstime' is invalid."	\
+: ((STATUS == ETIMEDOUT)						\
+? "The system time has reached or exceeded the time specified in "	\
+  "'abstime'."
+: "unknown"))
+
+
+/* pthread_condattr_t
+ * ========================================================================== */
+/* pthread_condattr_init */
+#define HANDLE_PTHREAD_CONDATTR_INIT(ATTR)				\
+do {									\
+	int status = pthread_condattr_init(ATTR);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to initialize pthread "		\
+				"condition attribute"			\
+				"\e24m]\n\n{\n"				\
+				"\tattr: '" #ATTR "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_CA_INIT_FAILURE(status));		\
+} while (0)
+#define PT_CA_INIT_FAILURE(STATUS)					\
+((STATUS == EAGAIN)							\
+? "Out of memory."							\
+: "unknown")
+
+
+/* pthread_condattr_destroy */
+#define HANDLE_PTHREAD_CONDATTR_DESTROY(ATTR)				\
+do {									\
+	int status = pthread_condattr_destroy(ATTR);			\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to destroy pthread condition "	\
+				"attribute"				\
+				"\e24m]\n\n{\n"				\
+				"\tattr: '" #ATTR "'\n"			\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_CA_DESTROY_FAILURE(status));		\
+} while (0)
+#define PT_CA_DESTROY_FAILURE(STATUS)					\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr'."						\
+: "unknown")
+
+
+/* if system supports the 'process-shared' or 'pshared' attribute */
+#ifdef _POSIX_THREAD_PROCESS_SHARED
+
+/* pthread_condattr_getpshared */
+#define HANDLE_PTHREAD_CONDATTR_GETPSHARED(ATTR, PSHARED)		\
+do {									\
+	int status = pthread_condattr_getpshared(ATTR, PSHARED);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to get 'process-shared' "	\
+				"for pthread condtion attribute"	\
+				"\e24m]\n\n{\n"				\
+				"\tattr:    '" #ATTR "'\n"		\
+				"\tpshared: '" #PSHARED "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_CA_GETPSHARED_FAILURE(status));	\
+} while (0)
+#define PT_CA_GETPSHARED_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "Invalid value for 'attr'."						\
+: "unknown")
+
+
+/* pthread_condattr_setpshared */
+#define HANDLE_PTHREAD_CONDATTR_SETPSHARED(ATTR, PSHARED)		\
+do {									\
+	int status = pthread_condattr_setpshared(ATTR, PSHARED);	\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set 'process-shared' "	\
+				"for pthread condition attribute"	\
+				"\e24m]\n\n{\n"				\
+				"\tattr:    '" #ATTR "'\n"		\
+				"\tpshared: '" #PSHARED "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_CA_SETPSHARED_FAILURE(status));	\
+} while (0)
+#define PT_CA_SETPSHARED_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "The new value specified for the attribute is outside the range of "	\
+  "legal values for that attribute."					\
+: "unknown")
+
+#endif	/* ifdef _POSIX_THREAD_PROCESS_SHARED */
+
+
+/* cancelability
+ * ========================================================================== */
+/* pthread_setcanceltype */
+#define HANDLE_PTHREAD_SETCANCELTYPE(TYPE, OLDTYPE)			\
+do {									\
+	int status = pthread_setcanceltype(TYPE, OLDTYPE);		\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set pthread cancel type"	\
+				"\e24m]\n\n{\n"				\
+				"\ttype:    '" #TYPE	"'\n"		\
+				"\toldtype: '" #OLDTYPE "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_SETCANCELTYPE_FAILURE(status));	\
+} while (0)
+#define PT_SETCANCELTYPE_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "The specified state is not 'PTHREAD_CANCEL_DEFERRED' or "		\
+  "'PTHREAD_CANCEL_ASYNCHRONOUS'."					\
+: "unknown")
+
+
+
+/* pthread_setcancelstate */
+#define HANDLE_PTHREAD_SETCANCELSTATE(TYPE, OLDSTATE)			\
+do {									\
+	int status = pthread_setcancelstate(TYPE, OLDSTATE);		\
+	if (status != 0)						\
+		EXIT_ON_FAILURE("failed to set pthread cancel state"	\
+				"\e24m]\n\n{\n"				\
+				"\ttype:     '" #TYPE	"'\n"		\
+				"\toldstate: '" #OLDSTATE "'\n"		\
+				"}\n\n"					\
+				"reason: %s",				\
+				PT_SETCANCELSTATE_FAILURE(status));	\
+} while (0)
+#define PT_SETCANCELSTATE_FAILURE(STATUS)				\
+((STATUS == EINVAL)							\
+? "The specified state is not 'PTHREAD_CANCEL_ENABLE' or "		\
+  "'PTHREAD_CANCEL_ASYNCHRONOUS'."					\
+: "unknown")
+
+/* FUNCTION-LIKE MACROS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
+
+#ifdef __cplusplus /* close 'extern "C" {' */
+}
+#endif
+
+#endif /* ifndef PARALLEL_HANDLE_H_ */
--- src/parallel/parallel.h
+++ src/parallel/parallel.h
@@ -11,236 +11,15 @@ extern "C" {
 
 /* EXTERNAL DEPENDENCIES ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
+#include <pthread.h>		/* pthead API */
 #include <utils/utils.h>	/* error handling, misc convenience fns */
 #include <utils/time.h>		/* real time clock, time types, structs, fns */
-#include <pthread.h>		/* pthead API */
+#include <parallel/handle.h>	/* pthead API error handler macros */
 
 /* EXTERNAL DEPENDENCIES ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
 
 
-/* FUNCTION-LIKE MACROS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
-
-/* error handlers
- * ========================================================================== */
-/* pthread_create */
-#define HANDLE_PTHREAD_CREATE(THREAD, ATTR, ROUTINE, ARG)		\
-do {									\
-	int status = pthread_create(THREAD, ATTR, ROUTINE, ARG);	\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to create pthread"		\
-				"\e24m]\n\n{\n"				\
-				"\tthread:        '" #THREAD  "'\n"	\
-				"\tattr:          '" #ATTR    "'\n"	\
-				"\tstart_routine: '" #ROUTINE "'\n"	\
-				"\targ:           '" #ARG     "'\n"	\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_CREATE_FAIL_RSN(status));		\
-} while (0)
-#define PT_CREATE_FAIL_RSN(STATUS)					\
-((STATUS == EAGAIN)							\
-? "The system lacked the necessary resources to create another thread"	\
-  ", or the system-imposed limit on the total number of threads in a "	\
-  "process [PTHREAD_THREADS_MAX] would be exceeded."			\
-: ((STATUS == EINVAL)							\
-? "The value specified by attr is invalid."				\
-: "unknown"))
-
-
-/* pthread_join */
-#define HANDLE_PTHREAD_JOIN(THREAD, VALUE_PTR)				\
-do {									\
-	int status = pthread_join(THREAD, VALUE_PTR);			\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to join pthread"		\
-				"\e24m]\n\n{\n"				\
-				"\tthread:    '" #THREAD    "'\n"	\
-				"\tvalue_ptr: '" #VALUE_PTR "'\n"	\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_JOIN_FAIL_RSN(status));		\
-} while (0)
-#define PT_JOIN_FAIL_RSN(STATUS)					\
-((STATUS == EDEADLK)							\
-? "A deadlock was detected or the value of thread specifies the "	\
-  "calling thread."							\
-: ((STATUS == EINVAL)							\
-? "The implementation has detected that the value specified by thread "	\
-  "does not refer to a joinable thread."				\
-: ((STATUS == ESRCH)							\
-? "No thread could be found corresponding to that specified by the "	\
-  "given thread ID, thread."						\
-: "unknown")))
-
-
-/* pthread_mutex_init */
-#define HANDLE_PTHREAD_MUTEX_INIT(MUTEX, ATTR)				\
-do {									\
-	int status = pthread_mutex_init(MUTEX, ATTR);			\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to initialize pthread mutex"	\
-				"\e24m]\n\n{\n"				\
-				"\tmutex: '" #MUTEX "'\n"		\
-				"\tattr:  '" #ATTR "'\n"		\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_MUTEX_INIT_FAIL_RSN(status));	\
-} while (0)
-#define PT_MUTEX_INIT_FAIL_RSN(STATUS)					\
-((STATUS == EAGAIN)							\
-? "The system temporarily lacks the resources to create another mutex."	\
-: ((STATUS == EINVAL)							\
-? "The value specified by 'attr' is invalid."				\
-: ((STATUS == ENOMEM)							\
-? "The process cannot allocate enough memory to create another mutex."	\
-: "unknown")))
-
-
-/* pthread_mutex_lock */
-#define HANDLE_PTHREAD_MUTEX_LOCK(MUTEX)				\
-do {									\
-	int status = pthread_mutex_lock(MUTEX);				\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to lock pthread mutex"		\
-				"\e24m]\n\n{\n"				\
-				"\tmutex: '" #MUTEX "'\n"		\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_MUTEX_LOCK_FAIL_RSN(status));	\
-} while (0)
-#define PT_MUTEX_LOCK_FAIL_RSN(STATUS)					\
-((STATUS == EDEADLK)							\
-? "A deadlock would occur if the thread is blocked waiting for mutex."	\
-: ((STATUS == EINVAL)							\
-? "The value specified by 'mutex' is invalid."				\
-: "unknown"))
-
-
-/* pthread_mutex_unlock */
-#define HANDLE_PTHREAD_MUTEX_UNLOCK(MUTEX)				\
-do {									\
-	int status = pthread_mutex_unlock(MUTEX);			\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to unlock pthread mutex"	\
-				"\e24m]\n\n{\n"				\
-				"\tmutex: '" #MUTEX "'\n"		\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_MUTEX_UNLOCK_FAIL_RSN(status));	\
-} while (0)
-#define PT_MUTEX_UNLOCK_FAIL_RSN(STATUS)				\
-((STATUS == EINVAL)							\
-? "The value specified by 'mutex' is invalid."				\
-: ((STATUS == EPERM)							\
-? "The current thread does not hold a lock on 'mutex'."			\
-: "unknown")
-
-
-/* pthread_cond_init */
-#define HANDLE_PTHREAD_COND_INIT(COND, ATTR)				\
-do {									\
-	int status = pthread_cond_init(COND, ATTR);			\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to set pthread cancel type"	\
-				"\e24m]\n\n{\n"				\
-				"\tcond: '" #COND "'\n"			\
-				"\tattr: '" #ATTR "'\n"			\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_COND_INIT_FAIL_RSN(status));		\
-} while (0)
-#define PT_COND_INIT_FAIL_RSN(STATUS)					\
-((STATUS == EAGAIN)							\
-? "The system temporarily lacks the resources to create another "	\
-  "condition variable."							\
-: ((STATUS == EINVAL)							\
-? "The value specified by 'attr' is invalid."				\
-: ((STATUS == ENOMEM)							\
-? "The process cannot allocate enough memory to create another "	\
-  "condition variable."							\
-: "unknown")))
-
-
-/* pthread_cond_signal */
-#define HANDLE_PTHREAD_COND_SIGNAL(COND)				\
-do {									\
-	int status = pthread_cond_signal(COND);				\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to signal pthread condition"	\
-				"\e24m]\n\n{\n"				\
-				"\tcond: '" #COND "'\n"			\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_COND_SIGNAL_FAIL_RSN(status));	\
-} while (0)
-#define PT_COND_SIGNAL_FAIL_RSN(STATUS)					\
-((STATUS == EINVAL)							\
-? "the value specified by 'cond' is invalid."				\
-: "unknown")
-
-
-/* pthread_cond_timedwait */
-#define HANDLE_PTHREAD_COND_TIMEDWAIT(COND, MUTEX, ABSTIME)		\
-do {									\
-	int status = pthread_cond_timedwait(COND, MUTEX, ABSTIME);	\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to signal pthread condition"	\
-				"\e24m]\n\n{\n"				\
-				"\tcond:    '" #COND "'\n"		\
-				"\tmutex:   '" #MUTEX "'\n"		\
-				"\tabstime: '" #ABSTIME "'\n"		\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_COND_TIMEDWAIT_FAIL_RSN(status));	\
-} while (0)
-#define PT_COND_TIMEDWAIT_FAIL_RSN(STATUS)				\
-((STATUS == EINVAL)							\
-? "The value specified by 'cond', 'mutex' or 'abstime' is invalid."	\
-: ((STATUS == ETIMEDOUT)						\
-? "The system time has reached or exceeded the time specified in "	\
-  "'abstime'."
-: "unknown"))
-
-
-/* pthread_condattr_setpshared */
-#define HANDLE_PTHREAD_CONDATTR_SETPSHARED(TYPE, OLDTYPE)		\
-do {									\
-	int status = pthread_setcanceltype(TYPE, OLDTYPE);		\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to set pthread cancel type"	\
-				"\e24m]\n\n{\n"				\
-				"\ttype:    '" #TYPE	"'\n"		\
-				"\toldtype: '" #OLDTYPE "'\n"		\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_SETCANCELTYPE_FAIL_RSN(status));	\
-} while (0)
-#define PT_SETCANCELTYPE_FAIL_RSN(STATUS)				\
-((STATUS == EINVAL)							\
-? "The specified state is not 'PTHREAD_CANCEL_DEFERRED' or "		\
-  "'PTHREAD_CANCEL_ASYNCHRONOUS'."					\
-: "unknown")
-
-
-/* pthread_setcanceltype */
-#define HANDLE_PTHREAD_SETCANCELTYPE(TYPE, OLDTYPE)			\
-do {									\
-	int status = pthread_setcanceltype(TYPE, OLDTYPE);		\
-	if (status != 0)						\
-		EXIT_ON_FAILURE("failed to set pthread cancel type"	\
-				"\e24m]\n\n{\n"				\
-				"\ttype:    '" #TYPE	"'\n"		\
-				"\toldtype: '" #OLDTYPE "'\n"		\
-				"}\n\n"					\
-				"reason: %s",				\
-				PT_SETCANCELTYPE_FAIL_RSN(status));	\
-} while (0)
-#define PT_SETCANCELTYPE_FAIL_RSN(STATUS)				\
-((STATUS == EINVAL)							\
-? "The specified state is not 'PTHREAD_CANCEL_DEFERRED' or "		\
-  "'PTHREAD_CANCEL_ASYNCHRONOUS'."					\
-: "unknown")
-
+/* TYPEDEFS, ENUM AND STRUCT DEFINITIONS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
 struct Task {
 	void *(*start_routine)(void *);
@@ -253,12 +32,23 @@ struct TimeoutArg {
 	int oldtype;
 };
 
-/* FUNCTION-LIKE MACROS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+/* TYPEDEFS, ENUM AND STRUCT DEFINITIONS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
 
 void *timeout_task(pthread_t *restrict thread,
 		   const pthread_attr_t *restrict attr,
 		   const struct Task *restrict task,
 		   const struct timespec *restrict timeout);
+
+/* convenience, default handlers
+ * ========================================================================== */
+inline void init_default_mutex(pthread_mutex_t *restrict mutex,
+			      pthread_mutexattr_t *restrict attr)
+{
+	HANDLE_PTHREAD
+}
+
+
 
 
 
