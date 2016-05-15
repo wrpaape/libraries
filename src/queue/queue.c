169ce4027bc4d88ec40a484155721662e868384c
Reid Paape <wrpaape@gmail.com>
2016-05-14 19:59:41 -0500

    queue module

---
+++ src/queue/queue.c
@@ -0,0 +1,10 @@
+#include <queue/queue.h>
+
+extern inline struct Queue *init_queue(void);
+
+extern inline void free_queue(struct Queue *queue);
+
+extern inline void queue_push(struct Queue *queue,
+			      void *data);
+
+extern inline void *queue_pop(struct Queue *queue);
---
+++ src/queue/queue.h
@@ -0,0 +1,115 @@
+#ifndef QUEUE_QUEUE_H_
+#define QUEUE_QUEUE_H_
+
+#ifdef __cplusplus /* ensure C linkage */
+extern "C" {
+#ifndef restrict /* replace 'restrict' with c++ compatible '__restrict__' */
+#define restrict __restrict__
+#endif
+#endif
+
+/* EXTERNAL DEPENDENCIES
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+#include <utils/utils.h>	/* malloc, free */
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * EXTERNAL DEPENDENCIES
+ *
+ *
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+struct QueueNode {
+	void *data;
+	struct QueueNode *prev;
+};
+
+struct Queue {
+	struct QueueNode *head;
+	struct QueueNode **last;
+};
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
+ *
+ *
+ * CONSTANTS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * CONSTANTS
+ *
+ *
+ * FUNCTION-LIKE MACROS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * FUNCTION-LIKE MACROS
+ *
+ *
+ * TOP-LEVEL FUNCTIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+inline struct Queue *init_queue(void)
+{
+	struct Queue *queue;
+	HANDLE_MALLOC(queue, sizeof(struct Queue));
+
+	queue->head = NULL;
+	queue->last = NULL;
+
+	return queue;
+}
+
+inline void free_queue(struct Queue *queue)
+{
+	free(queue);
+}
+
+inline void queue_push(struct Queue *queue,
+		       void *data)
+{
+	struct QueueNode *node;
+	HANDLE_MALLOC(node, sizeof(struct QueueNode));
+
+	node->data = data;
+	node->prev = NULL;
+
+	if (queue->last == NULL) {
+		queue->head = node;
+	} else {
+		*(queue->last) = node;
+	}
+
+	queue->last = &(node->prev);
+}
+
+inline void *queue_pop(struct Queue *queue)
+{
+	struct QueueNode *node = queue->head;
+
+	if (node == NULL)
+		return NULL;
+
+	void *data = node->data;
+
+	queue->head = node->prev;
+
+	free(node);
+
+	return data;
+}
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * TOP-LEVEL FUNCTIONS
+ *
+ *
+ * HELPER FUNCTIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * HELPER FUNCTIONS */
+
+#ifdef __cplusplus /* close 'extern "C" {' */
+}
+#endif
+
+#endif /* ifndef QUEUE_QUEUE_H_ */
