155e3d737926d0fd2bff3bc85e2b8592fb6e58fb
Reid Paape <wrpaape@gmail.com>
2016-05-17 10:35:45 -0500

    moving modules around

---
+++ src/nlp/src/nlg.h
@@ -0,0 +1,142 @@
+#ifndef NLP_NLG_H_
+#define NLP_NLG_H_
+
+#ifdef __cplusplus /* ensure C linkage */
+extern "C" {
+#ifndef restrict /* replace 'restrict' with c++ compatible '__restrict__' */
+#define restrict __restrict__
+#endif
+#endif
+
+
+/* EXTERNAL DEPENDENCIES
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+#include <rand/rand.h>	/* random number generator */
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * EXTERNAL DEPENDENCIES
+ *
+ *
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+struct NounPhrase {
+};
+
+struct VerbPhrase {
+	char *main;
+	char *aux;
+};
+
+struct Clause {
+	struct NounPhrase *noun_phrase;
+	struct VerbPhrase *verb_phrase;
+};
+
+enum VerbType {
+	INTRANSITIVE = 0,
+	TRANSITIVE   = 1,
+	DITRANSITIVE = 2
+};
+
+enum Mood {
+	INDICATIVE  = 0,
+	SUBJUNCTIVE = 1,
+	CONDITIONAL = 2
+};
+
+
+enum Plurality {
+	SINGULAR = 0,
+	PLURAL	 = 1
+};
+
+
+enum Polarity {
+	AFFIRMATIVE = 0,
+	NEGATIVE    = 1
+};
+
+
+enum Gender {
+	MASCULINE = 0,
+	FEMININE  = 1,
+	NEUTRAL	  = 2
+};
+
+
+#define PERSONAL_PRONOUN_TYPES 5
+enum PersonalPronounType {
+	SUBJECT		       = 0,
+	OBJECT		       = 1,
+	DEPENDENT_POSSESSIVE   = 2,
+	INDEPENDENT_POSSESSIVE = 3,
+	REFLEXIVE	       = 4
+};
+
+#define PERSON_TYPES 3
+enum GrammaticalPerson {
+	FIRST  = 0,
+	SECOND = 1,
+	THIRD  = 2
+};
+
+#define NUMBER_TYPES 2
+enum GrammaticalNumber {
+	SINGULAR = 0,
+	PLURAL	 = 1
+};
+
+#define TENSE_TYPES 3
+enum GrammaticalTense {
+	PAST	= 0,
+	PRESENT	= 1,
+	FUTURE	= 2
+};
+
+struct WordGroup {
+	char **words;
+	size_t i_max;
+};
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
+ *
+ *
+ * CONSTANTS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * CONSTANTS
+ *
+ *
+ * FUNCTION-LIKE MACROS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * FUNCTION-LIKE MACROS
+ *
+ *
+ * TOP-LEVEL FUNCTIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * TOP-LEVEL FUNCTIONS
+ *
+ *
+ * HELPER FUNCTIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+inline char *sample_word_group(const struct WordGroup *const group)
+{
+	return group->words[ (group->i_max == 0ul)
+			     ? 0ul
+			     : rand_uint_upto(group->i_max) ];
+}
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * HELPER FUNCTIONS */
+
+#ifdef __cplusplus /* close 'extern "C" {' */
+}
+#endif
+
+#endif /* ifndef NLP_NLG_H_ */
---
+++ src/nlp/src/nlp.c
@@ -0,0 +1,6 @@
+#include "nlp.h"
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
