903574e459ae82956cd9d66228177382cc0c62a7
Reid Paape <wrpaape@gmail.com>
2016-05-23 00:53:38 -0500

    exporting all headers by default for 'USER_LIBRARIES'

---
+++ include/string_utils/string_utils.h
@@ -0,0 +1,87 @@
+#ifndef STRING_UTILS_STRING_UTILS_H_
+#define STRING_UTILS_STRING_UTILS_H_
+
+#ifdef __cplusplus /* ensure C linkage */
+extern "C" {
+#ifndef restrict /* replace 'restrict' with c++ compatible '__restrict__' */
+#define restrict __restrict__
+#endif
+#endif
+
+
+/* EXTERNAL DEPENDENCIES
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+#include <ctype.h>		/* toupper */
+#include "ascii.h"		/* ascii_t */
+#include "split_string.h"	/* split_string, <string.h>, <utils/utils.h> */
+/* #include "generate.h"	/1* string generators *1/ */
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * EXTERNAL DEPENDENCIES
+ *
+ *
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
+ *
+ *
+ * CONSTANTS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * CONSTANTS
+ *
+ *
+ * FUNCTION-LIKE MACROS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * FUNCTION-LIKE MACROS
+ *
+ *
+ * TOP-LEVEL FUNCTIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+inline char *extend_string(char *restrict buffer,
+			   const char *restrict extend)
+{
+	while (*extend != '\0') {
+		*buffer = *extend;
+		++buffer;
+		++extend;
+	}
+
+	return buffer;
+}
+
+inline char *capitalize_string(const char *restrict string)
+{
+	char *restrict cap_string;
+
+	const size_t length = strlen(string);
+
+	HANDLE_MALLOC(cap_string,
+		      sizeof(char) * (length + 1ul));
+
+	for (size_t i = 0ul; i < length; ++i)
+		cap_string[i] = (char) toupper((int) string[i]);
+
+	cap_string[length] = '\0';
+
+	return cap_string;
+}
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * TOP-LEVEL FUNCTIONS
+ *
+ *
+ * HELPER FUNCTIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * HELPER FUNCTIONS */
+
+#ifdef __cplusplus /* close 'extern "C" {' */
+}
+#endif
+
+#endif /* ifndef STRING_UTILS_STRING_UTILS_H_ */
---
+++ include/utils/utils.h
@@ -0,0 +1,135 @@
+#ifndef UTILS_UTILS_H_
+#define UTILS_UTILS_H_
+
+#ifdef __cplusplus /* ensure C linkage */
+extern "C" {
+#ifndef restrict /* replace 'restrict' with c++ compatible '__restrict__' */
+#define restrict __restrict__
+#endif
+#endif
+
+
+/* EXTERNAL DEPENDENCIES ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+#include <stdlib.h>	/* m/c/realloc, free, exit */
+#include <stddef.h>	/* ptrdiff_t */
+#include <stdio.h>	/* printf, fprintf */
+#include <errno.h>	/* errno */
+#include <string.h>	/* strerror */
+#include <limits.h>	/* CHAR_BIT */
+
+/* EXTERNAL DEPENDENCIES ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
+
+/* FUNCTION-LIKE MACROS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+#define BIT_SIZE(TYPE) (sizeof(TYPE) * CHAR_BIT)
+
+#define EXIT_ON_FAILURE(FORMAT, ...)					\
+do {									\
+	fprintf(stderr,							\
+		"\n\e[31m\e[5mERROR:\e[25m\t\e[4m" FORMAT "\e[24m\n\n"	\
+		"errno: %s\n\n"						\
+		"file:  %s\n\n"						\
+		"func:  %s\n\n"						\
+		"line:  %d\e[0m\n",					\
+		##__VA_ARGS__,						\
+		strerror(errno), __FILE__, __func__, __LINE__);		\
+	exit(EXIT_FAILURE);						\
+} while (0)
+
+/* error handlers
+ * ========================================================================== */
+#ifdef __cplusplus /* for c++ files */
+/* memory allocation (cast returned alloc, 'nullptr' instead of 'NULL') */
+#define HANDLE_MALLOC(PTR, SIZE)					\
+do {									\
+	PTR = (__typeof__(PTR)) malloc(SIZE);				\
+	if (PTR == nullptr)						\
+		EXIT_ON_FAILURE("failed to allocate %lu bytes of "	\
+				"memory for '" #PTR "'", SIZE);		\
+} while (0)
+#define HANDLE_CALLOC(PTR, COUNT, SIZE)					\
+do {									\
+	PTR = (__typeof__(PTR)) calloc(COUNT, SIZE);			\
+	if (PTR == nullptr)						\
+		EXIT_ON_FAILURE("failed to allocate %lu blocks of %lu"	\
+				"bytes of zeroed memory for '" #PTR	\
+				"'", COUNT, SIZE);			\
+} while (0)
+#define HANDLE_REALLOC(PTR, SIZE)					\
+do {									\
+	PTR = (__typeof__(PTR)) realloc(PTR, SIZE);			\
+	if (PTR == nullptr)						\
+		EXIT_ON_FAILURE("failed to reallocate memory for '"	\
+				#PTR "' to %lu bytes", SIZE);		\
+} while (0)
+
+
+#else	/* for c files */
+
+/* memory allocation */
+#define HANDLE_MALLOC(PTR, SIZE)					\
+do {									\
+	PTR = malloc(SIZE);						\
+	if (PTR == NULL)						\
+		EXIT_ON_FAILURE("failed to allocate %lu bytes of "	\
+				"memory for '" #PTR "'", SIZE);		\
+} while (0)
+#define HANDLE_CALLOC(PTR, COUNT, SIZE)					\
+do {									\
+	PTR = calloc(COUNT, SIZE);					\
+	if (PTR == NULL)						\
+		EXIT_ON_FAILURE("failed to allocate %lu blocks of %lu"	\
+				"bytes of zeroed memory for '" #PTR	\
+				"'", COUNT, SIZE);			\
+} while (0)
+#define HANDLE_REALLOC(PTR, SIZE)					\
+do {									\
+	PTR = realloc(PTR, SIZE);					\
+	if (PTR == NULL)						\
+		EXIT_ON_FAILURE("failed to reallocate memory for '"	\
+				#PTR "' to %lu bytes", SIZE);		\
+} while (0)
+
+#endif	/* ifdef __cplusplus */
+
+
+/* returns 'THIS' or 'THAT' according to the predicate 'THIS CMP THAT',
+ * where 'CMP' is a binary relational operator */
+#define THIS_OR_THAT(THIS, THAT, CMP) (((THIS) CMP (THAT)) ? (THIS) : (THAT))
+
+/* returns minimum of 'X' and 'Y' */
+#define MIN(X, Y) THIS_OR_THAT(X, Y, <)
+
+/* returns maximum of 'X' and 'Y' */
+#define MAX(X, Y) THIS_OR_THAT(X, Y, >)
+
+
+/* print elements of 'ARRAY' */
+#define PRINT_ARRAY(ARRAY, LENGTH, FORMAT)		\
+do {							\
+	printf(#ARRAY ": {\n" FORMAT, ARRAY[0l]);	\
+	for (ptrdiff_t i = 1; i < LENGTH; ++i)		\
+		printf(", " FORMAT, ARRAY[i]);		\
+	puts("\n}");					\
+} while (0)
+
+/* FUNCTION-LIKE MACROS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
+
+inline size_t next_pow_two(const size_t num)
+{
+	return 1lu << (BIT_SIZE(size_t) - __builtin_clzl(num - 1lu));
+}
+
+inline size_t log_base_two(const size_t num)
+{
+	return __builtin_ctzl(num);
+}
+
+#ifdef __cplusplus /* close 'extern "C" {' */
+}
+#endif
+
+#endif /* ifndef UTILS_UTILS_H_ */
