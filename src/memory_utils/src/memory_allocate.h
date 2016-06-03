2578c97e78c0c53d9beaeb8fd96c4ff7b4b77020
Reid Paape <wrpaape@gmail.com>
2016-06-03 16:22:26 -0500

    mmap instead of 'deprecated' sbrk

--- src/memory_utils/src/memory_allocate.h
+++ src/memory_utils/src/memory_allocate.h
@@ -12,8 +12,10 @@ extern "C" {
 /* EXTERNAL DEPENDENCIES
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
-#include "memory_utils.h"	/* Width<WIDTH> */
+/* #include "memory_utils.h"	/1* Width<WIDTH> *1/ */
 #include <unistd.h>		/* size_t, brk, sbrk, PAGE_SIZE */
+#include <sys/mman.h>		/* */
+
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * EXTERNAL DEPENDENCIES
@@ -22,7 +24,12 @@ extern "C" {
  * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
+struct MemoryAllocator {
+	size_t offset;
+};
+
 struct Chunk {
+	size_t *length;
 	void *buffer;
 };
 
@@ -45,6 +52,7 @@ struct Chunk {
 /* #endif /1* ifndef PAGESIZE *1/ */
 /* #endif /1* ifndef PAGE_SIZE *1/ */
 
+static size_t *alloc_space = 0;
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * CONSTANTS
@@ -53,28 +61,64 @@ struct Chunk {
  * FUNCTION-LIKE MACROS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
+mmap(0, alloc_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, VM_MAKE_TAG(VM_MEMORY_MALLOC), 0);
+
 
 #define GET_PAGE_SIZE() sysconf(_SC_PAGESIZE)
+mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
 
-#define HANDLE_MORE_CORE(BUFFER, ALLOC)					\
+#define HANDLE_MMAP(BUFFER, ADDR, LEN, PROT, FLAGS, FD, OFFSET)		\
 do {									\
-	BUFFER = sbrk(ALLOC);						\
-	if (((ptrdiff_t) BUFFER) == -1)					\
+	BUFFER = mmap(ADDR, LEN, PROT, FLAGS, FD, OFFSET);		\
+	if (BUFFER == MAP_FAILED)					\
 		EXIT_ON_FAILURE("failed to allocate %zu additional "	\
 				"bytes of memory onto the heap"		\
-				"\e24m]\n\n"				\
-				"reason: %s",				\
-				MORE_CORE_FAILURE(errno));		\
+				"\e24m]\n\n{\n"				\
+				"\taddr:   '" #ADDR   "',\n"		\
+				"\tlen:    '" #LEN    "',\n"		\
+				"\tprot:   '" #PROT   "',\n"		\
+				"\tflags:  '" #FLAGS  "',\n"		\
+				"\tfd:     '" #FD     "',\n"		\
+				"\toffset: '" #OFFSET "'\n"		\
+				"}\n\n"					\
+				"mmap failure: %s",			\
+				LEN,					\
+				MMAP_FAILURE(errno));			\
 } while (0)
-#define MORE_CORE_FAILURE(STATUS)					\
-  ((STATUS == ENOMEM)
-? "sbrk failure of one the following:\n"
-  "\tThe limit, as set by setrlimit(2), was exceeded.\n"
-  "\tThe maximum possible size of a data segment (compiled into the "
-  "system) was exceeded.\n"
-  "\tInsufficient space existed in the swap area to support the "
-  "expansion.\n"
-: "unknown sbrk failure\n")
+#define MMAP_FAILURE(ERRNO)					\
+  ((ERRNO == EACCES)
+? "(one of the following)\n"
+? "\t- The flag 'PROT_READ' was specified as part of the 'prot' "
+  "argument and 'fd' was not open for reading.\n"
+  "\t- The flags 'MAP_SHARED' and 'PROT_WRITE' were specified as part "
+  "of the flags and 'prot' argument and 'fd' was not open for writing."
+
+: ((ERRNO == EBADF)
+? "The 'fd' argument is not a valid open file descriptor."
+  ((ERRNO == EINVAL)
+? "(one of the following)\n"
+  "\t- 'MAP_FIXED' was specified and the addr argument was not page "
+  "aligned, or part of the desired address space resides out of "
+  "the valid address space for a user process.\n"
+  "\t- 'flags' does not include either 'MAP_PRIVATE' or 'MAP_SHARED'\n"
+  "\t- The 'len' argument was negative or zero.\n"
+  "\t- The 'offset' argument was not page-aligned based on the page "
+  "size as returned by 'getpagesize(3)'.
+  ((ERRNO == ENODEV)
+? "'MAP_ANON' has not been specified and the file 'fd' refers to does "
+  "not support mapping.
+  ((ERRNO == ENOMEM)
+? "(one of the following)\n"
+  "'MAP_FIXED' was specified and the addr argument was not available.  MAP_FIXED was specified and the
+		address range specified exceeds the address space limit for the process.  MAP_ANON was specified
+		and insufficient memory was available.
+
+  ((ERRNO == ENXIO)
+? "Addresses in the specified range are invalid for fd.
+
+  ((ERRNO == EOVERFLOW)
+? "Addresses in the specified range exceed the maximum offset set for fd.
+: "unknown")
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * FUNCTION-LIKE MACROS
