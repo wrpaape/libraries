dc815a52a760f7da89de98dd11f507d3b81b52ea
Reid Paape <wrpaape@gmail.com>
2016-06-03 17:35:45 -0500

    finally found where PAGE_SIZE was hiding

--- src/memory_utils/src/memory_allocate.h
+++ src/memory_utils/src/memory_allocate.h
@@ -12,9 +12,10 @@ extern "C" {
 /* EXTERNAL DEPENDENCIES
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
-/* #include "memory_utils.h"	/1* Width<WIDTH> *1/ */
-#include <unistd.h>		/* size_t, brk, sbrk, PAGE_SIZE */
-#include <sys/mman.h>		/* */
+#include <unistd.h>		/* sysconf, _SC_PAGE_SIZE */
+#include <sys/mman.h>		/* size_t, mmap */
+#include <mach/vm_statistics.h>	/* PAGE_SIZE, VM_MAKE_TAG, VM_MEMORY_MALLOC */
+
 
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
@@ -25,7 +26,7 @@ extern "C" {
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
 struct MemoryAllocator {
-	size_t offset;
+	off_t offset;
 };
 
 struct Chunk {
@@ -44,14 +45,6 @@ struct Chunk {
 #define CHUNK_SIZE (1ul << 4)
 #endif /* ifndef CHUNK_SIZE */
 
-/* #ifndef PAGE_SIZE */
-/* #ifdef PAGESIZE */
-/* #define PAGE_SIZE PAGESIZE */
-/* #else */
-/* #define PAGE_SIZE 4096ul */
-/* #endif /1* ifndef PAGESIZE *1/ */
-/* #endif /1* ifndef PAGE_SIZE *1/ */
-
 static size_t *alloc_space = 0;
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
@@ -61,11 +54,26 @@ static size_t *alloc_space = 0;
  * FUNCTION-LIKE MACROS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
-mmap(0, alloc_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, VM_MAKE_TAG(VM_MEMORY_MALLOC), 0);
+/* mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset); */
+/* mmap(0, alloc_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, VM_MAKE_TAG(VM_MEMORY_MALLOC), 0); */
 
 
 #define GET_PAGE_SIZE() sysconf(_SC_PAGESIZE)
-mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+
+#define MMAP_PROT PROT_READ | PROT_WRITE
+#define MMAP_FLAGS MAP_ANON | MAP_PRIVATE
+#define MMAP_FD VM_MAKE_TAG(VM_MEMORY_MALLOC)
+#define MMAP_ADDR ((void *) 0)
+#define MMAP_OFFSET ((off_t) 0)
+
+#define HANDLE_MORE_CORE(BUFFER, LEN)		\
+HANDLE_MMAP(BUFFER,				\
+	    (void *) 0,				\
+	    LEN,				\
+	    PROT_READ | PROT_WRITE,		\
+	    MAP_ANON | MAP_PRIVATE,		\
+	    VM_MAKE_TAG(VM_MEMORY_MALLOC),	\
+	    (off_t) 0)
 
 #define HANDLE_MMAP(BUFFER, ADDR, LEN, PROT, FLAGS, FD, OFFSET)		\
 do {									\
@@ -85,40 +93,40 @@ do {									\
 				LEN,					\
 				MMAP_FAILURE(errno));			\
 } while (0)
-#define MMAP_FAILURE(ERRNO)					\
-  ((ERRNO == EACCES)
-? "(one of the following)\n"
-? "\t- The flag 'PROT_READ' was specified as part of the 'prot' "
-  "argument and 'fd' was not open for reading.\n"
-  "\t- The flags 'MAP_SHARED' and 'PROT_WRITE' were specified as part "
-  "of the flags and 'prot' argument and 'fd' was not open for writing."
-
-: ((ERRNO == EBADF)
-? "The 'fd' argument is not a valid open file descriptor."
-  ((ERRNO == EINVAL)
-? "(one of the following)\n"
-  "\t- 'MAP_FIXED' was specified and the addr argument was not page "
-  "aligned, or part of the desired address space resides out of "
-  "the valid address space for a user process.\n"
-  "\t- 'flags' does not include either 'MAP_PRIVATE' or 'MAP_SHARED'\n"
-  "\t- The 'len' argument was negative or zero.\n"
-  "\t- The 'offset' argument was not page-aligned based on the page "
-  "size as returned by 'getpagesize(3)'.
-  ((ERRNO == ENODEV)
-? "'MAP_ANON' has not been specified and the file 'fd' refers to does "
-  "not support mapping.
-  ((ERRNO == ENOMEM)
-? "(one of the following)\n"
-  "'MAP_FIXED' was specified and the addr argument was not available.  MAP_FIXED was specified and the
-		address range specified exceeds the address space limit for the process.  MAP_ANON was specified
-		and insufficient memory was available.
-
-  ((ERRNO == ENXIO)
-? "Addresses in the specified range are invalid for fd.
-
-  ((ERRNO == EOVERFLOW)
-? "Addresses in the specified range exceed the maximum offset set for fd.
-: "unknown")
+#define MMAP_FAILURE(ERRNO)						\
+  ((ERRNO == EACCES)							\
+? "(one of the following)\n"						\
+  "\t- The flag 'PROT_READ' was specified as part of the 'prot' "	\
+  "argument and 'fd' was not open for reading.\n"			\
+  "\t- The flags 'MAP_SHARED' and 'PROT_WRITE' were specified as part "	\
+  "of the flags and 'prot' argument and 'fd' was not open for writing."	\
+: ((ERRNO == EBADF)							\
+? "The 'fd' argument is not a valid open file descriptor."		\
+: ((ERRNO == EINVAL)							\
+? "(one of the following)\n"						\
+  "\t- 'MAP_FIXED' was specified and the addr argument was not page "	\
+  "aligned, or part of the desired address space resides out of "	\
+  "the valid address space for a user process.\n"			\
+  "\t- 'flags' does not include either 'MAP_PRIVATE' or 'MAP_SHARED'\n"	\
+  "\t- The 'len' argument was negative or zero.\n"			\
+  "\t- The 'offset' argument was not page-aligned based on the page "	\
+  "size as returned by 'getpagesize(3)'."				\
+: ((ERRNO == ENODEV)							\
+? "'MAP_ANON' has not been specified and the file 'fd' refers to does "	\
+  "not support mapping."						\
+: ((ERRNO == ENOMEM)							\
+? "(one of the following)\n"						\
+  "\t- 'MAP_FIXED' was specified and the 'addr' argument was not "	\
+  "available.\n"							\
+  "\t- 'MAP_FIXED' was specified and the address range specified "	\
+  "exceeds the address space limit for the process.\n"			\
+  "\t- 'MAP_ANON' was specified and insufficient memory was available."	\
+: ((ERRNO == ENXIO)							\
+? "Addresses in the specified range are invalid for 'fd'."		\
+: ((ERRNO == EOVERFLOW)							\
+? "Addresses in the specified range exceed the maximum offset set for "	\
+  "'fd'."								\
+: "unknown")))))))
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * FUNCTION-LIKE MACROS
